0933b794c37d702a28775b01ec1057872b407883
niklas sandberg <niklas.sandberg@gmail.com>
2021-12-10 15:44:00 +0100

    Ny version 16:44

--- src/main/java/MonsterIIImain.java
+++ src/main/java/MonsterIIImain.java
@@ -8,8 +8,6 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-
-
 public class MonsterIIImain {
     public static void main(String[] args) throws Exception {
         TerminalSize ts = new TerminalSize(100, 120);
@@ -18,94 +16,56 @@ public class MonsterIIImain {
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
 
-        int x = 5;
-        int y = 5;
+        int x = 50;
+        int y = 42;
+        int wallSize = 40;
         final char player = 'X';
         final char block = '\u2588';
-        final char bomb = 'O';
+
         terminal.setCursorPosition(x, y);
         terminal.putCharacter(player);
-        String wall = String.valueOf(block);
-        wall = wall.repeat(10);
-
-
-        //create obstacles array
-        Position[] obstacles = new Position[10];
-        for (int i = 0; i<10; i++) {
-            obstacles[i] = new Position(10+i, 10);
-        }
-        // use obstacles array to print to lanterna
-        for (Position p : obstacles) {
-            terminal.setCursorPosition(p.x, p.y);
-            terminal.putCharacter(block);
-        }
-
-
-        // Try to put obstacle in different places tar bort 1 block random
-        Random ro = new Random();/*
-        Position obstaclePosition = new Position(ro.nextInt(120), ro.nextInt(100));
-        terminal.setCursorPosition(obstaclePosition.x, obstaclePosition.y);
-        terminal.putCharacter(block);
-           */
-          int randomKolumn = ro.nextInt(110);
-/*
-        for (int i = 0; i < wall.length(); i++) {
-            terminal.setCursorPosition(randomKolumn+i, 0);
-            terminal.putCharacter(wall.charAt(i));
-        }*/
-        terminal.flush();
-
-        /*
-        //create obstacles array2
-        Position[] obstacles2 = new Position[10];
-        for (int i = 0; i<10; i++) {
-            obstacles2[i] = new Position(ro.nextInt(80), ro.nextInt(80));
-        }
-        // use obstacles array2 to print to lanterna
-        for (Position o : obstacles2) {
-            terminal.setCursorPosition(ro.nextInt(80), ro.nextInt(24));
-            terminal.putCharacter(block);
-        }
-*/
-        //Task 12
-        Random r = new Random();
-        Position bombPosition = new Position(r.nextInt(80), r.nextInt(24));
-        terminal.setCursorPosition(bombPosition.x, bombPosition.y);
-        terminal.putCharacter(bomb);
 
         terminal.flush();
-        //Moving wall
+
         int i = 0;
         int j = 0;
-        int rok = ThreadLocalRandom.current().nextInt(100);
+        int rok = ThreadLocalRandom.current().nextInt(100-wallSize);
+        int rok2 = ThreadLocalRandom.current().nextInt(100-wallSize);
 
-        Position[] wallPositions = new Position[10];
+        Position[] wallPositions = new Position[wallSize];
 
         boolean continueReadingInput = true;
         while (continueReadingInput) {
             KeyStroke keyStroke = null;
 
             do {
-                Thread.sleep(5); // might throw InterruptedException
+                Thread.sleep(5);
                 keyStroke = terminal.pollInput();
                 if (i % 100 == 0) {
                     if (j < 100) {
-                        for (int k = 0; k < 10; k++) {
+                        for (int k = 0; k < wallSize; k++) {
                             terminal.setCursorPosition(rok + k, j);
                             terminal.putCharacter(block);
                             terminal.setCursorPosition(rok + k, j - 1);
                             terminal.putCharacter(' ');
                             wallPositions[k] = new Position(rok + k, j);
-                            //wallPositions[k].setX(rok+k);
-                            //wallPositions[k].setY(j);
                         }
+
                         j++;
-                        //System.out.println(j);
+                        //påbörjar kod för en andra wall slutar rad 63
+                        for (int k = 0; k < wallSize; k++) {
+                            terminal.setCursorPosition((rok2 + k, j);
+                            terminal.putCharacter(block);
+                            terminal.setCursorPosition(rok2 + k, j - 1);
+                            terminal.putCharacter(' ');
+                            wallPositions[k] = new Position(rok2 + k, j);
+                        }
+
                         for (Position go : wallPositions) {
 
                             if (go.getX() == x && go.getY() == y) {
                                 System.out.println("Game Over");
-                                //Ändring:
+
                                 continueReadingInput = false;
                                 break;
                             }
@@ -118,17 +78,14 @@ public class MonsterIIImain {
                         terminal.flush();
                     }
 
-
                 }
                 i++;
             } while (keyStroke == null);
-
 
             if (keyStroke != null) {
                 KeyType type = keyStroke.getKeyType();
                 Character c = keyStroke.getCharacter();
 
-                //System.out.println("keyStroke.getKeyType: " + type + "keyStroke.getCharacter(): " + c);
                 if (c == Character.valueOf('q')) {
                     continueReadingInput = false;
                     terminal.close();
@@ -150,32 +107,14 @@ public class MonsterIIImain {
                         x -= 1;
                         break;
                 }
-                //detect if player tries to run into obstacle
-                boolean chrashIntoObstacle = false;
-                for (Position p : obstacles) {
-                    if (p.x == x && p.y == y) {
-                        chrashIntoObstacle = true;
-                    }
-                }
-                if (chrashIntoObstacle) {
-                    x = oldX;
-                    y = oldY;
-                } else {
-
                     terminal.setCursorPosition(oldX, oldY);
                     terminal.putCharacter(' ');
                     terminal.setCursorPosition(x, y);
                     terminal.putCharacter(player);
-                }
-                //check if player runs into bomb
-                if (bombPosition.x == x && bombPosition.y == y) {
-                    terminal.close();
-                }
-                terminal.flush();
 
+                terminal.flush();
             }
         }
-
         terminal.setCursorPosition(25,25);
         String messageGO = "GAME OVER. YOU LOOSE!";
         for (int ind = 0; ind < messageGO.length(); ind++) {
@@ -183,12 +122,6 @@ public class MonsterIIImain {
         }
 
         terminal.flush();
-
-
-        // Byt ut "B" till att loopa över message
-
-
-
     }
 
 }
